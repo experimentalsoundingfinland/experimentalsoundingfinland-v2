048d4c9f324d6b9068c29b4b52ed629ad8b5fc4c
Libero Mureddu <experimentalsoundingfinland@gmail.com>
2025-02-18 13:34:54 +0200

    Update gcal_fetch_events.rb

--- assets/rb/gcal_fetch_events.rb
+++ assets/rb/gcal_fetch_events.rb
@@ -12,19 +12,37 @@ API_KEY = ENV["GOOGLE_API_KEY"]
 CALENDAR_ID = ENV["GOOGLE_CALENDAR_ID"]
 url = URI("https://www.googleapis.com/calendar/v3/calendars/#{CALENDAR_ID}/events?singleEvents=true&key=#{API_KEY}")
 
-response = Net::HTTP.get(url)
-events = JSON.parse(response)['items']
+begin
+  response = Net::HTTP.get(url)
+  puts "API response: #{response}"
+  data = JSON.parse(response)
+  events = data['items']
+  puts "Events: #{events}"
+rescue StandardError => e
+  puts "Error: #{e.message}"
+  exit 1
+end
+
+if events.nil? || events.empty?
+  puts "No events found"
+  exit 0
+end
 
 # Ensure the _posts directory exists
 FileUtils.mkdir_p('_posts')
 
 events.each do |event|
-  date = DateTime.now
   event_start = DateTime.parse(event['start']['dateTime'] || event['start']['date'])
   event_end = DateTime.parse(event['end']['dateTime'] || event['end']['date'])
   title = event['summary'].gsub(/[^0-9A-Za-z.\-]/, '-').downcase
-  filename = "_posts/#{date.strftime('%Y-%m-%d')}-#{title}.md"
+  filename = "_posts/#{event_start.strftime('%Y-%m-%d')}-#{title}.md"
   
+  # Check if file already exists
+  if File.exist?(filename)
+    puts "File already exists for event: #{event['summary']}"
+    next
+  end
+
   # Extract venue from the end of the description
   description = event['description'] || 'No description available.'
   venue_marker = 'event_venue123'
@@ -51,7 +69,7 @@ events.each do |event|
     ---
     layout: post
     title: "#{event['summary']}"
-    date: #{date.iso8601}
+    date: #{event_start.iso8601}
     event_start: #{event_start.iso8601}
     event_end: #{event_end.iso8601}
     venue: "#{venue}"
@@ -62,4 +80,5 @@ events.each do |event|
   CONTENT
 
   File.write(filename, content)
+  puts "Generated file for event: #{event['summary']}"
 end
