593fd4b235d8c7e4da36f35cf010a032c3198bbd
Libero Mureddu <experimentalsoundingfinland@gmail.com>
2025-03-11 11:08:24 +0200

    Update gcal_fetch_events.rb

--- assets/rb/gcal_fetch_events.rb
+++ assets/rb/gcal_fetch_events.rb
@@ -95,8 +95,8 @@ events.each do |event|
   DTSTART;TZID=Europe/Helsinki:#{event_start.strftime('%Y%m%dT%H%M%S')}
   DTEND;TZID=Europe/Helsinki:#{event_end.strftime('%Y%m%dT%H%M%S')}
   SUMMARY:#{event['summary']}
-  DESCRIPTION:#{description}
-  LOCATION:"#{event['location'] || 'TBD'}"
+  DESCRIPTION:#{venue}
+  LOCATION:#{event['location'] || 'TBD'}
   URL:https://experimentalsoundingfinland.github.io/experimentalsoundingfinland-v2/#{date_str}/#{id_portion}.html
   END:VEVENT
   END:VCALENDAR
